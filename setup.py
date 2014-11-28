b0e2615e5dd0b5f86ce62b370ff2b63b4cdd62bf
Carles Bruguera <carlesba@gmail.com>
2014-11-28 07:49:23 +0100

    Package boilerplate

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(name='maxutils',
+      version=version,
+      description="Common utilities for mx software family",
+      long_description="""\
+A collection of methods and classes subject to be used accross all max components""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='max utils python',
+      author='Carles Bruguera',
+      author_email='carles.bruguera@upcnet.es',
+      url='https://github.com/UPCnet/maxutils',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
